627ed3e5220af6f7da320c1410d00573b9268afd
unknown <kvitriseptia@gmail.com>
2025-05-20 21:00:07 +0700

    menambhakan model product

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,9 @@
 const User = require("./user");
+const Product = require("./product");
 
 const initializeTables = async () => {
-    await User.sync();
+    await User.sync({ alter: true });
+    await Product.sync({ alter: true });
 };
 
 initializeTables();
---
+++ src/models/product.js
@@ -0,0 +1,42 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../configs/database");
+
+const Product = sequelize.define(
+  "product",
+  {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { type: DataTypes.STRING, allowNull: false },
+    image: { type: DataTypes.STRING },
+    description: { type: DataTypes.TEXT },
+    benefit: { type: DataTypes.TEXT },
+    ingredient: { type: DataTypes.TEXT },
+    instruction: { type: DataTypes.TEXT },
+    link_url: { type: DataTypes.STRING },
+    category: {
+      type: DataTypes.ENUM(
+        "micellar water",
+        "face wash",
+        "toner",
+        "serum",
+        "moisturizer",
+        "sunscreen"
+      ),
+      allowNull: false,
+    },
+    skin_type_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "skin_types",
+        key: "id",
+      },
+    },
+  },
+  {
+    tableName: "products",
+    timestamps: true,
+    paranoid: true,
+    underscored: true,
+  }
+);
+
+module.exports = Product;
