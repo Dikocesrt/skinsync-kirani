eb8048fee2838f3378cf0a20c7c8bf989d988f4d
unknown <kvitriseptia@gmail.com>
2025-05-20 21:29:41 +0700

    menambahkan express

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
+const express = require("express");
+
 require("./configs/database");
 require("./models/index");
 require("./configs/cloudinary");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
