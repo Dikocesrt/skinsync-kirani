dca626ac4263f6a826002eb99b354528d52370f1
unknown <kvitriseptia@gmail.com>
2025-05-20 21:07:46 +0700

    menambahkan relasi table

--- src/models/index.js
+++ src/models/index.js
@@ -4,6 +4,20 @@ const SkinType = require("./skinType");
 const History = require("./history");
 const Article = require("./article");
 
+User.hasMany(History, { foreignKey: "user_id" });
+History.belongsTo(User, { foreignKey: "user_id" });
+
+SkinType.hasMany(History, { foreignKey: "skin_type_id" });
+History.belongsTo(SkinType, { foreignKey: "skin_type_id" });
+
+SkinType.hasMany(Product, { foreignKey: "skin_type_id" });
+Product.belongsTo(SkinType, { foreignKey: "skin_type_id" });
+
+SkinType.hasMany(Article, { foreignKey: "skin_type_id" });
+Article.belongsTo(SkinType, { foreignKey: "skin_type_id" });
+
+
+
 
 const initializeTables = async () => {
     await User.sync({ alter: true });
@@ -17,4 +31,8 @@ initializeTables();
 
 module.exports = {
     User,
+    Product,
+    SkinType,
+    History,
+    Article
 };
