8d38a5eca77a5052d3a140f59e018b749716b255
unknown <kvitriseptia@gmail.com>
2025-05-20 21:54:55 +0700

    setup template engine & static file

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
+// PORT DEPEDENCY
 const express = require("express");
-
+const path = require("path");
+const hbs = require("hbs"); 
+require("dotenv").config();
 require("./configs/database");
 require("./models/index");
 require("./configs/cloudinary");
 
+// MEMBUAT WEB SERVER
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+// RUN APLIKASI
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
